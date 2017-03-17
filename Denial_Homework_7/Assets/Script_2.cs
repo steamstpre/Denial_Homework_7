79079c6a327a4392f137d8877b9876a32d2d7921
denial <denialwins6@gmail.com>
2017-03-17 21:31:17 +0200

    denial_homework_7

---
+++ Denial_Homework_7/Assets/Script_2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Script_2 : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() { }
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private bool grounded = false;
+
+    Rigidbody GetComponent<Rigidbody>;
+    void FixedUpdate()
+    {
+        if (grounded)
+        { }
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+
+        if (Input.GetButton("Jump"))
+        {
+moveDirection.y = jumpSpeed;
+        }
+    }
+}
+// Update is called once per frame
+void Update()
+{
+    float horizontal = Input.GetAxis("Horizontal");
+    float vertical = Input.GetAxis("Vertical");
+    Vector3 direction = new Vector3(horizontal, 0f, vertical);
+    Rigidbody.addforce(direction * speed);
+}
